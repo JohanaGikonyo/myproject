9c9bf05f4bfe6f186b6107b227ad66ffeca1fb2f
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-02-06 15:25:57 +0300

    new6

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -6,9 +6,16 @@ function App() {
   const [num2, setNum2] = useState('');
   const [percent, setPercent] = useState('');
   const [showRedBox, setShowRedBox] = useState(false);
- const [quiz, setQuiz]=useState(false)
+  const [quiz, setQuiz] = useState('')
   const percentage = () => {
-    setPercent((num1 / num2) * 100);
+    if (quiz==="percentageOf"){
+      setPercent((num1 / num2) * 100);
+    }
+   else{
+    setPercent((num1/100)*num2)
+   } 
+    
+    
   };
 
   const clear = () => {
@@ -25,66 +32,94 @@ function App() {
   return (
     <div className='cal2'>
       {/* <h1 className='btn btn-block'>Calculation 2</h1> */}
-      {
-        quiz?<div>
-
-          
-        </div>
-        
-        :
-         <div>
-        <h4>Fabric Shrinkage Calculator</h4>
-        <div className='select1'>
-          <input type="text" placeholder="Enter fabric" />
-          <select name="" id="" placeholder="select Wash Type" value="Select Wash Type">
-            <option></option>
-          </select>
-        </div>
-        <div className='select2'>
-          <label>Select Calculation Type:</label>
-          <select >
-            <option value="">X is what percentage of Y</option>
-            <option value="">What is X percentage of Y</option>
-          </select>
-        </div>
-        <input type="number" onChange={(e) => setNum1(e.target.value)} value={num1} />
-        <br />
-        <label>is what % of </label>
-        <br />
-        <input type="number" onChange={(e) => setNum2(e.target.value)} value={num2} />
-        <p style={{color:"red"}}>Answer {` ${percent} %`}</p>
-        <div className="box-container"> {/* Container for the boxes */}
-          <div className='d-flex '><p className='m-3'>{num2}</p>
-          { num1 && num2 && ( // Render red box if showRedBox is true and num1 and num2 are provided
-              <div
-                className="box" /* Apply both classes */
-                style={{ height: `${num2}vh`,width: `${num2}vh` }}
-              ></div>
-            )}
-
-         
-          
-          </div> {/* Original blue box */}
-
-          {showRedBox && num1 && num2 && ( // Render red box if showRedBox is true and num1 and num2 are provided
-            <div
-              className="box red-box" /* Apply both classes */
-              style={{ width: `${(((num1 / num2) * 100) / 100) * num2}vh` , height:`${num2}vh`}}
-            >{num1}</div>
-          )}
-        </div>
-        <div className='submits'>
-          <button onClick={clear} className='btn btn-danger'>CLEAR</button>
-          <button onClick={percentage} className='btn btn-primary'>CALCULATE</button>
-        </div>
-        <div className='submits'>
-          <button className='btn btn-dark'>PRINT</button>
-          <button className='btn btn-success' onClick={review}>PREVIEW</button>
-        </div>
-      </div>
-      }
       
-     
+          <div>
+            <h4>Fabric Shrinkage Calculator</h4>
+            <div className='select1'>
+              <input type="text" placeholder="Enter fabric" />
+              <select name="" id="" placeholder="select Wash Type" value="Select Wash Type">
+                <option></option>
+              </select>
+            </div>
+            <div className='select2'>
+              <label>Select Calculation Type:</label>
+              <select onChange={(e)=>{setQuiz(e.target.value)}}>
+                <option value="percentageOf">X is what percentage of Y</option>
+                <option value="percentage">What is X percentage of Y</option>
+              </select>
+            </div>
+
+            {
+        (quiz==='percentageOf')?
+         (<div>
+<input type="number" onChange={(e) => setNum1(e.target.value)} value={num1} />
+            <br />
+            <label>is what % of </label>
+            <br />
+            <input type="number" onChange={(e) => setNum2(e.target.value)} value={num2} />
+            <p style={{ color: "red" }}>Answer {` ${percent} %`}</p>
+            <div className="box-container"> {/* Container for the boxes */}
+              <div className='d-flex '><p className='m-3'>{num2}</p>
+                {num1 && num2 && ( // Render red box if showRedBox is true and num1 and num2 are provided
+                  <div
+                    className="box" /* Apply both classes */
+                    style={{ height: `${num2}vh`, width: `${num2}vh` }}
+                  ></div>
+                )}
+
+
+
+              </div> {/* Original blue box */}
+
+              {showRedBox && num1 && num2 && ( // Render red box if showRedBox is true and num1 and num2 are provided
+                <div
+                  className="box red-box" /* Apply both classes */
+                  style={{ width: `${(((num1 / num2) * 100)/100)*num2}vh`, height: `${num2}vh` }}
+                >{num1}</div>
+              )}
+            </div>
+        </div>
+  )
+          :
+            (<div>
+               <label>What is</label>
+<input type="number" onChange={(e) => setNum1(e.target.value)} value={num1} /><p>% Of</p>
+            <br />
+           
+            <br />
+            <input type="number" onChange={(e) => setNum2(e.target.value)} value={num2} />
+            <p style={{ color: "red" }}>Answer {` ${percent} `}</p>
+            <div className="box-container"> {/* Container for the boxes */}
+              <div className='d-flex '><p className='m-3'>{num2}</p>
+                {num1 && num2 && ( // Render red box if showRedBox is true and num1 and num2 are provided
+                  <div
+                    className="box" /* Apply both classes */
+                    style={{ height: `${num2}vh`, width: `${num2}vh` }}
+                  ></div>
+                )}</div> 
+ {/* Original blue box */}
+
+              {showRedBox && num1 && num2 && ( // Render red box if showRedBox is true and num1 and num2 are provided
+                <div
+                  className="box red-box" /* Apply both classes */
+                  style={{ width: `${percent}vh`, height: `${num2}vh` }}
+                >{num1}</div>
+              )}
+            </div>
+            </div>)
+}
+            <div className='submits'>
+              <button onClick={clear} className='btn btn-danger'>CLEAR</button>
+              <button onClick={percentage} className='btn btn-primary'>CALCULATE</button>
+            </div>
+            <div className='submits'>
+              <button className='btn btn-dark'>PRINT</button>
+              <button className='btn btn-success' onClick={review}>PREVIEW</button>
+            </div>
+          </div>
+      
+
+
     </div>
   );
 }
